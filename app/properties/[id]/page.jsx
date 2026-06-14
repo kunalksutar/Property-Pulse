a2e3aac870a5865d2887185f9934991fd6c08964
Kunal Kiran Sutar <kunalksutar@gmail.com>
2026-06-14 21:19:02 +0530

    experimenting with hooks and useRouter

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+'use client';
+import React from 'react';
+import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
 
 const PropertyPage = () => {
+  const router = useRouter();
+  const {id} = useParams();
+
+  const searchParams = useSearchParams();
+  const name = searchParams.get('name');
+
+  const pathname = usePathname();
+
+  console.log("hello")
+  
   return (
     <div>
-        Property Page
+        <button onClick = {() => router.push('/')}> Go Home {pathname} {name}</button>
     </div>
   )
 }
