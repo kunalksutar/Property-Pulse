4be8e4ddd0c6ad925ab4d254ac6ea878bb3b53d2
Kunal Kiran Sutar <kunalksutar@gmail.com>
2026-06-16 20:48:41 +0530

    Created mongodb schemas for User and Property

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,8 @@ import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
 
-import connectDB from '@/config/database';
+const HomePage = () => {
 
-const HomePage = async () => {
-  console.log(process.env.MONGODB_URI);
-
-  await connectDB();
   return (
     <div>
         <Hero></Hero>
