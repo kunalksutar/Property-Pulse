7382cab424caf6c6a3a3c9724837cd7d710c219b
Kunal Kiran Sutar <kunalksutar@gmail.com>
2026-06-23 21:52:00 +0530

    updated the Google OAuth endpoint. Next step : wrap the entire codebase in OAuth

---
+++ utils/authOptions.js
@@ -0,0 +1,33 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export default authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                prompt: "consent",
+                access_type: "offline",
+                response_type: "code"
+                }
+            }
+        })
+    ], 
+    callbacks: {
+        // 1. invoked on successful signin
+        async SignIn({ profile }) {
+            // 1. connect to the db
+            // 2. check if user exists
+            // 3. if not, then add user to db
+            // 4. return true to allow sign in 
+        },
+
+        // 2. Modify the session object
+        async session({ session }) {
+            // 1. get user from db
+            // 2. assign the user id to the session
+            // 3. return session 
+        }
+    }
+};
